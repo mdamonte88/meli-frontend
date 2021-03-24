1b6c662d4cb9034c53e358bcfe93e910849e32ed
Matias Damonte <mdamonte88@hotmail.com>
2021-03-24 15:14:24 -0300

    Removes unnecesaries elements at the HomePage

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,43 +1,14 @@
 import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
-import { getItems } from '../store/actions/item'
 
 class HomePage extends PureComponent {
 
-  componentWillMount() {
-    this.props.loadItems();
-  }
-
-  renderItems(props) { 
-    const items = props.map( item => {
-      return <li key={item._id}> {item.title}- U$S {item.price} </li>
-    })
-
-    return <ul> { items } </ul>;
-  }
-
-
   render() {
-    const { itemsList } = this.props;
-
     return (
-      <div className="slides-container homepage">
-        Home Page
-        {this.renderItems(itemsList)} 
-      </div>
+      <div className="main-container homepage"> </div>
     );
   }
 }
 
-const mapState = state => {
-    return ({
-      itemsList: state.items.itemsList.toJS()
-    });
-  }
+
   
-  
-  const mapDispatch = dispatch => ({
-    loadItems: () => dispatch(getItems())
-  });
-  
-  export default connect(mapState, mapDispatch)(HomePage);
+export default HomePage;
