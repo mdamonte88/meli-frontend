7193683abec590ec7f74721f43413b7b31d5660d
Matias Damonte <mdamonte88@hotmail.com>
2021-03-24 14:25:10 -0300

    Modified the Header component in order to redirect after to search in this
    Applies some changes in the header stylles
    Adds withRouter to use the history and location urls

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
 
 // Estilos
 import './Header.scss';
 import logo from '../../assets/Logo_ML.png';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        };
+    }
+
+    goHome() {
+        this.props.history.push('/');
+    }
+
+    goSearchPage() {
+        const { query } = this.state;
+        const url = '/items/search/'+ query
+        this.props.history.push(url);
+    }
+
+    handleOnchange(e) {
+        const value = e.nativeEvent.target.value;
+        this.setState(() => ({ query: value }) );
+    }
+
     render() {
         return (
-            <header className="App-header" style={{minHeight: '50px', maxHeight: '150px'}}>
-                <img src={logo} className="App-logo" alt="logo" />
+            <header className="app-header" style={{minHeight: '50px', maxHeight: '150px'}}>
+                 <img src={logo} className="app-logo" alt="logo" onClick={() => this.goHome()} />
+                
                 <input 
                     type="text" 
-                    value="Nunca dejes de buscar" 
-                    class="input-search"
-                    autocomplete="off"
-                    autocorrect="off"
-                    autocapitalize="none"
-                    spellcheck="false"
-                    placeholder="Buscar"
+                    className = "search-input"
+                    placeholder="Nunca dejes de buscar"
+                    onChange={(e) => this.handleOnchange(e)}
                 />
+                <button className="search-button" type="submit" onClick={() => this.goSearchPage()}/>
             </header>
         );
     }
 }
 
-export default Header;
+export default withRouter(Header);
