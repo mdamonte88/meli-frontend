84e50486ab03cc24f299fa13a7db22ab209e4a8f
Matias Damonte <mdamonte88@hotmail.com>
2021-03-30 14:00:15 -0300

    Converts Homepage to stateless component or functional component

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,12 +1,9 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 
-class HomePage extends PureComponent {
-
-  render() {
-    return (
-      <div className="main-container homepage"> </div>
-    );
-  }
+const HomePage = () => {
+  return (
+    <div className="main-container homepage"> </div>
+  );
 }
-  
+
 export default HomePage;
